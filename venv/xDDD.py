4d705c5f96dc08c8eedda992bfaec6150195819e
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-12-20 00:22:10 +0100

    Xd

--- venv/xDDD.py
+++ venv/xDDD.py
@@ -6,10 +6,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 gChromeOptions = webdriver.ChromeOptions()
 gChromeOptions.add_argument("window-size=1920x1480")
 gChromeOptions.add_argument("disable-dev-shm-usage")
+gChromeOptions.add_argument("headless")
 gDriver = webdriver.Chrome(
     chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
 )
 gDriver.get("https://www.python.org/")
 time.sleep(3)
 gDriver.save_screenshot("my_screenshot.png")
+print(gDriver.get_screenshot_as_png("my_screenshot.png"));
 gDriver.close()
