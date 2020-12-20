6288933d601186c10176abd3e862fde918e95666
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-12-20 14:03:34 +0100

    deploy

--- venv/xDDD.py
+++ venv/xDDD.py
@@ -1,16 +1,3 @@
-# import time
-# print("HELLO LOGS XD ");
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-#
-# gChromeOptions = webdriver.ChromeOptions()
-# gChromeOptions.set_headless()
-# gChromeOptions.add_argument("window-size=1920x1480")
-# gChromeOptions.add_argument("disable-dev-shm-usage")
-# gDriver = webdriver.Chrome(
-#     chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
-# )
-# print("EMPTY");
 from selenium import webdriver
 import os
 chromeoptions = webdriver.ChromeOptions();
@@ -20,5 +7,6 @@ chromeoptions.add_argument("--disable-dev-shm-usage")
 chromeoptions.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chromeoptions)
 
-driver.get("https://www.google.com")
+driver.get("https://www.familysearch.org/pl/")
 print(driver.page_source)
+driver.quit()
