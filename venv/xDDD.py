a7f198c665edbd403359536e3a35aa751ee801ec
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-12-20 18:25:38 +0100

    resolution

--- venv/xDDD.py
+++ venv/xDDD.py
@@ -3,6 +3,7 @@ import os
 chromeoptions = webdriver.ChromeOptions();
 chromeoptions.binary_location = os.environ.get("GOOGLE_CRHOME_BIN")
 chromeoptions.add_argument("--headless")
+chromeoptions.add_argument("--window-size=1920x1080")
 chromeoptions.add_argument("--disable-dev-shm-usage")
 chromeoptions.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chromeoptions)
