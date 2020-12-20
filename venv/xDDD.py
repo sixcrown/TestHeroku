5d3e656d44f61df9838db0664fd695ba4bd5aae0
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-12-20 17:53:22 +0100

    click 2 iteration

--- venv/xDDD.py
+++ venv/xDDD.py
@@ -8,7 +8,7 @@ chromeoptions.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chromeoptions)
 
 driver.get("https://www.familysearch.org/")
-sign_in = driver.find_element_by_xpath('//*[@id="signInLink"]');
+sign_in = driver.find_element_by_xpath('/html/body/div/header/div[2]/div[2]/nav/a[1]');
 sign_in.click();
 print(driver.page_source)
 driver.quit()
