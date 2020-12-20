762a0eb7cb4c999c11a95cc21a630f8e3a63f1a3
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-12-20 18:20:40 +0100

    accept cookies

--- venv/xDDD.py
+++ venv/xDDD.py
@@ -7,11 +7,11 @@ chromeoptions.add_argument("--disable-dev-shm-usage")
 chromeoptions.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chromeoptions)
 
-driver.get("https://www.familysearch.org/")
-print(driver.page_source)
+driver.get("https://www.familysearch.org/en/")
+driver.implicitly_wait(2)
 acceptCookies = driver.find_element_by_xpath('/html/body/div[8]/div[1]/div/div[2]/div[2]/a[1]')
 acceptCookies.click();
-sign_in = driver.find_element_by_xpath('/html/body/div/header/div[2]/div[2]/nav/a[1]');
-sign_in.click()
-print(driver.page_source)
+# sign_in = driver.find_element_by_xpath('/html/body/div/header/div[2]/div[2]/nav/a[1]');
+# sign_in.click()
+# print(driver.page_source)
 driver.quit()
